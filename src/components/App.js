49ba18c34a6235f1c15d749d7d29faf1490b809d
Ky <kyrill@gobber.ch>
2021-09-28 23:52:15 +0200

    Oh man, what have I done

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import Featured from "./Featured";
 
 
 const App = () => (
-    <BrowserRouter>
+    <BrowserRouter basename="/course-directory">
       <div className="container">
           <Header/>
           <Switch>
